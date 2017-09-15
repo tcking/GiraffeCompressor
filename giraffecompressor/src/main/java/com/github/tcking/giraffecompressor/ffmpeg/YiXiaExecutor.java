fa0a67e232a6d16da0cb803ac7c2e5a602662afe
tcking <mytcking@gmail.com>
2017-09-15 11:56:06 +0800

    add new ffmpeg imple

--- giraffecompressor/src/main/java/com/github/tcking/giraffecompressor/ffmpeg/YiXiaExecutor.java
+++ giraffecompressor/src/main/java/com/github/tcking/giraffecompressor/ffmpeg/YiXiaExecutor.java
@@ -1,6 +1,9 @@
 package com.github.tcking.giraffecompressor.ffmpeg;
 
 import android.content.Context;
+import android.os.Build;
+
+import com.yixia.videoeditor.adapter.UtilityAdapter;
 
 /**
  * Created by TangChao on 2017/9/14.
@@ -19,7 +22,6 @@ public class YiXiaExecutor implements FFMPEGCmdExecutor {
 
     @Override
     public void init(Context context) {
-//        UtilityAdapter.FFmpegInit(context, String.format("versionName=%s&versionCode=%d&sdkVersion=%s&android=%s&device=%s",
-//                "1.0", 1, "1.2.0", "com.tcking.videocompressor", Build.MODEL==null?"":Build.MODEL));
+        UtilityAdapter.FFmpegInit(context, String.format("versionName=%s&versionCode=%d&sdkVersion=%s&android=%s&device=%s", "1.0", 1, "1.2.0", "com.github.tcking.giraffecompressorr", Build.MODEL==null?"":Build.MODEL));
     }
 }
